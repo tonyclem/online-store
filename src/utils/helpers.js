25bf4c35984e645c4e1b95450d371896737fcced
tonyclem <tonyclem001@gmail.com>
2022-07-06 21:12:04 +0200

    added featured product and format price

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,8 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+  return new Intl.NumberFormat("en-US", {
+    currency: "USD",
+    style: "currency",
+  }).format(number / 100);
+};
 
-export const getUniqueValues = () => {}
+export const getUniqueValues = () => {};
