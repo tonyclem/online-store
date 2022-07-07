105af82c263a8d21f2beec5a117a5169f21e5d55
tonyclem <tonyclem001@gmail.com>
2022-07-07 13:47:44 +0200

    added controlled input value

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -8,6 +8,8 @@ const Sort = () => {
     grid_view,
     setListView,
     setGridView,
+    sort,
+    updateSort,
   } = useFilterContext();
 
   return (
@@ -34,7 +36,13 @@ const Sort = () => {
       <hr />
       <form>
         <label htmlFor="sort">sort by</label>
-        <select name="sort" id="sort" className="sort-input">
+        <select
+          name="sort"
+          id="sort"
+          className="sort-input"
+          value={sort}
+          onChange={updateSort}
+        >
           <option value="price-lowest">price (lowest)</option>
           <option value="price-highest">price (highest)</option>
           <option value="name-a">name (a - z)</option>
